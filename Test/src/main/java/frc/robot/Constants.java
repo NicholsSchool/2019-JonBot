825e4e1f3e2515a949768c09f220af274792f38e
Arnav Parashar <Arnav@Arnavs-Air.nicholsschool.org>
2019-01-10 16:44:09 -0500

    init

---
+++ Test/src/main/java/frc/robot/Constants.java
@@ -0,0 +1,12 @@
+package frc.robot;
+
+public class Constants{
+    public static final int MOTOR1VAL = 34;
+
+    public static final int 
+     LFMASTER_ID = 26, LFSLAVE_ID = 24,
+     RFMASTER_ID = 25, RFSLAVE_ID = 23,
+     LBMASTER_ID = 28, LBSLAVE_ID = 30,
+     RBMASTER_ID = 27, RBSLAVE_ID = 29;
+
+}
